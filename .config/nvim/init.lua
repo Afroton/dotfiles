1a48550464a5e92487d33e6dd43b64df353980de
Afroton <orsonbecker@gmail.com>
2024-05-02 16:13:43 -0500

    started .files stow repo

---
+++ .config/nvim/init.lua
@@ -0,0 +1,32 @@
+
+require("orson.core.options")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Basic Keymaps 
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+require("lazy").setup({
+
+{
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+}
+})
+
+
+
